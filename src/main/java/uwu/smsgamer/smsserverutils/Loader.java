b92b7e9a227e1044daa128a0410753b9086fa41b
true-cc <creepercannyon@gmail.com>
2021-02-12 13:54:09 -0800

    Updated PacketEvents and removed jython from Loader.java

--- src/main/java/uwu/smsgamer/smsserverutils/Loader.java
+++ src/main/java/uwu/smsgamer/smsserverutils/Loader.java
@@ -20,9 +20,7 @@ public class Loader extends JavaPlugin {
         final Lilliputian lilliputian = new Lilliputian(this);
         lilliputian.getDependencyBuilder()
           .addDependency(new Dependency(Repository.JITPACK,
-            "com.github.retrooper", "packetevents", "v1.8-pre-3"))
-          .addDependency(new Dependency(Repository.MAVENCENTRAL,
-            "org.python", "jython-standalone", "2.7.2"))
+            "com.github.retrooper", "packetevents", "v1.8-pre-4"))
           .addDependency(new Dependency(Repository.MAVENCENTRAL,
             "org.xerial", "sqlite-jdbc", "3.8.11.2"))
           .loadDependencies();
